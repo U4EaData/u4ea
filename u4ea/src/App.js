c31b78472f31ff995823fbd95eb783d446092746
Kota Soda <kota_soda@brown.edu>
2020-07-12 14:33:59 -0400

    setup

--- u4ea/src/App.js
+++ u4ea/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
+/* import components here */
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hello</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        
+      </Switch>
+    </Router>
   );
 }
 
